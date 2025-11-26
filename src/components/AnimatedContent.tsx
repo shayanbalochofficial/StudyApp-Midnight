54d1cb9bcfb109d8cba9d70822afb3110fe1df81
Shayan Baloch <shayanbalochofficial@gmail.com>
2025-11-27 02:53:15 +0500

    Creating Home Page

---
+++ src/components/AnimatedContent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const AnimatedContent = () => {
+  return (
+    <motion.div className="z-10 backdrop-blur-lg bg-black/30"></motion.div>
+  );
+};
+
+export default AnimatedContent;
