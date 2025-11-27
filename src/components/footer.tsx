71ad9b717ef5a999bb08e7e4b92fcda03e9ecfa6
Shayan Baloch <shayanbalochofficial@gmail.com>
2025-11-27 22:59:32 +0500

    Created Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const footer = () => {
+const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className="bg-black/50 backdrop-blur-md py-4 mt-auto">
+      <div className="container mx-auto px-4 text-center">
+        <p className="text-sm text-gray-400">© 2025 Shayan Baloch</p>
+      </div>
+    </footer>
+  );
+};
 
-export default footer
+export default Footer;
