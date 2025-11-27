75e70ccdcee25db7bc2ee0ebfad2124fe8dc96e8
Shayan Baloch <shayanbalochofficial@gmail.com>
2025-11-27 05:56:28 +0500

    Building Navbar (header)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
 
-const header = () => {
+const Header = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <header className="bg-black/50 backdrop-blur-md fixed top-0 left-0 right-0 z-50 ">
+      <nav className="">
+        <Link>StudyPro</Link>
+        <div>
+          <Button>
+            <Link>To-do</Link>
+          </Button>
+          <Button>
+            <Link>Countdown</Link>
+          </Button>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default header
+export default Header;
