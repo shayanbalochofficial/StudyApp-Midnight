0fec4b3e211dde5b2b51a9bd33ea3cc98699cf0a
Shayan Baloch <shayanbalochofficial@gmail.com>
2025-11-28 16:55:13 +0500

    Solving Todo Page Errors + Building Todo Page

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,11 +1,38 @@
-"use state"
-const page = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+"use state";
+
+import { useState } from "react";
+
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+  subject: string;
 }
 
-export default page
+const subjects = [
+  "Mathematics",
+  "Computer Science",
+  "Physics",
+  "Chemistry",
+  "Biology",
+  "English",
+  "Business",
+  "other",
+];
 
+const page = () => {
+  const [todos, setTodos] = useState<Todo[]>(() => {
+    if (typeof window !== "undefined") {
+      // sharing all ur secrets with each other since we were kids, got the music can u baby tell me y
+    }
+  });
+
+  return (
+    <div className="max-w-4xl mx-auto mt-16 p-8 bg-gray-900/50 backdrop-blur-lg rounded-2xl shadow-2xl">
+      <h1 className="">Your To-do List</h1>
+      <div className="">{/* Subjects Slection Function here */}</div>
+    </div>
+  );
+};
+
+export default page;
