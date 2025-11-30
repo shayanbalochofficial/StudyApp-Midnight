6e1faa8a510d4576867f80c7c7dd97777a3937ce
Shayan Baloch <shayanbalochofficial@gmail.com>
2025-11-30 22:39:44 +0500

    Building Countdown

--- src/components/countdowncontent.tsx
+++ src/components/countdowncontent.tsx
@@ -1,9 +1,37 @@
-import React from "react";
+"use client";
+import { useToast } from "@/hooks/use-toast";
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+const targetDate = new Date("2026-05-06T00:00:00");
+function calculateTimeLeft() {
+  const difference = +targetDate - +new Date();
 
-function calclateTimeLeft() {}
+  let timeLeft = {};
+
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  }
+
+  // vibe coding
+  return timeLeft;
+}
 
 const countdowncontent = () => {
-  return <div></div>;
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  const { toast } = useToast();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+  });
+
+  return <motion.div></motion.div>;
 };
 
 export default countdowncontent;
